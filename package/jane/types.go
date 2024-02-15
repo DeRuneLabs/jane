3b9f9b0aac1e10ae0cf000246fbff4202044260d
arfy slowy <slowy.arfy@gmail.com>
2024-02-15 21:47:55 +0700

    chore: adding support int16
    Signed-off-by: arfy slowy <slowy.arfy@gmail.com>

---
+++ package/jane/types.go
@@ -0,0 +1,13 @@
+package jane
+
+const (
+	Void   uint = 0
+	Int8   uint = 1
+	Int16  uint = 2
+	Int32  uint = 3
+	Int64  uint = 4
+	UInt8  uint = 5
+	UInt16 uint = 6
+	UInt32 uint = 7
+	UInt64 uint = 8
+)
